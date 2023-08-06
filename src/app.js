ed5299f8fdc1acfb1b2d864040725f04e506cbc1
Diego Vite Hernández <undecode0@hotmail.com>
2023-08-05 21:49:21 -0600

    Category (CRUD) and createRecipe: created

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { createRoles } from './libs/initialSetup';
 
 // Routes
 import authRouter from './routes/auth.routes.js';
+import categoryRouter from './routes/category.routes';
+import recipeRouter from './routes/recipe.routes';
 
 const app = express();
 createRoles();
@@ -12,6 +14,8 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // Routes
-app.use('/auth', authRouter)
+app.use('/auth', authRouter);
+app.use('/category', categoryRouter);
+app.use('/recipe', recipeRouter);
 
 export default app;
